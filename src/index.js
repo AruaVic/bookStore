e71031f453ca10c077f56f46536101dd9564ddf5
zhangya7570 <Kevin7570@163.com>
2018-04-26 10:56:46 +0800

    1.modify webpackConfig to correct webpack-build;2.modify template of index.html so that semanticUI used normally

--- src/index.js
+++ src/index.js
@@ -3,16 +3,23 @@ import React from 'react';
 export default class defaultComponent extends React.Component {
     render() {
         return (
-            <div className="shopping-list">
-                <h1>Shopping List for name</h1>
-                <ul>
-                    <li>Instagram</li>
-                    <li>WhatsApp</li>
-                    <li>Oculus</li>
-                    <li>Oculus</li>
-                    <li>Oculus</li>
+            <div>
+                <div className="shopping-list">
+                    <h1>Shopping List for name</h1>
+                    <ul>
+                        <li>Instagram</li>
+                        <li>WhatsApp</li>
+                        <li>Oculus</li>
+                        <li>WhatsApp</li>
+                        <li>WhatsApp</li>
+                        <li>Oculus</li>
 
-                </ul>
+                    </ul>
+                </div>
+                <button className="ui labeled icon button"><i className="pause icon"></i> Pause </button>
+                <button className="ui right labeled icon button"><i className="right arrow icon"></i> Next </button>
+                <button className="ui primary button">Save </button>
+                <button className="ui button">Discard </button>
             </div>
         );
     }
